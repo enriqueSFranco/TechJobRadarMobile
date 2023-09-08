e1637c68ff3b4072288af5dfd24d68aec21fefe5
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-09-08 15:07:58 -0600

    feat: Add component header

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import { Platform, StyleSheet, View } from "react-native"
+
+interface HeaderProps {
+  children: React.ReactNode
+}
+
+export const Header: React.FC<HeaderProps> = ({ children }) => {
+  return (
+    <View style={styles.header}>
+      {children}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  header: {
+    paddingTop: Platform.OS === 'android' ? 30 : 0
+  }
+})
