769f42bc665e0ac921e87f9bc5a3ddb010d5cf0b
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-09-07 19:00:51 -0600

    feat: add new title app

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App () {
   return (
     <View style={styles.container}>
-      <Text>Hellow world</Text>
+      <Text>Hellow world 🌎</Text>
       <StatusBar style="auto" />
     </View>
   );
