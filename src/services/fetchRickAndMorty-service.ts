88fd92bf5b70726ee2618a3c65b6f36b788a3866
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-09-10 15:50:03 -0600

    feat: Add utils fetch

--- src/services/fetchRickAndMorty-service.ts
+++ src/services/fetchRickAndMorty-service.ts
@@ -1,34 +1,17 @@
+import { fetcher } from "../helpers/fetcher-util"
 import { RickAndMorty } from "../shared"
 
 const BASE_URL = 'https://rickandmortyapi.com/api'
 
 export const fetchRickAndMorty = async (): Promise<RickAndMorty> => {
-  const controller = new AbortController()
-  const { signal } = controller
-
   try {
-    const url = new URL(`${BASE_URL}/character`)
-    const response = await fetch(url.pathname, { signal })
-
-    if (signal.aborted) {
-      throw new Error('Solicitud cancelada')
-    }
-
-    if (!response.ok) {
-      throw new Error(`Error ${response.status}: ${response.statusText}`)
-    }
-
-    const data: RickAndMorty = await response.json()
+    const data: RickAndMorty = await fetcher(`${BASE_URL}/character`)
 
     return data
   } catch (error) {
     if (error instanceof Error) {
-      if (error.name === 'AbortError') {
-        throw new Error('La solicitud fue cancelada')
-      } else {
-        throw new Error(`Error: ${error.message}`)
-      }
+      throw new Error(`Error: ${error.message}`)
     }
-    throw new Error('Error desconocido')
+    return Promise.reject(error)
   }
 }
