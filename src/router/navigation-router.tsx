071ed6db09da367806a6816e347bcf98e04b4d6e
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-09-18 18:03:17 -0600

    feat: add main app

--- src/router/navigation-router.tsx
+++ src/router/navigation-router.tsx
@@ -1,30 +1,46 @@
-import { NavigationContainer, ParamListBase, RouteProp } from '@react-navigation/native'
+import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createDrawerNavigator } from '@react-navigation/drawer'
 import { Ionicons } from '@expo/vector-icons'
 import { Feed } from '../screens/feed'
 import { Notifications } from '../screens/notifications'
 import { Settings } from '../screens/settings'
+import { TweetDetail } from '../screens/tweet-detail'
+import { Tweet } from '../shared'
 
-type AppStackParamList = {
+type StackParamList = {
+  TabGroup: undefined
+  TweetDetail: Tweet
+}
+
+type TabGroupStackParamList = {
   Feed: undefined
   Notifications: undefined
   Settings: undefined
 }
 
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator<StackParamList>()
 
-const Tab = createBottomTabNavigator<AppStackParamList>()
+const Tab = createBottomTabNavigator<TabGroupStackParamList>()
+
+const Drawer = createDrawerNavigator()
 
 function StackGroup () {
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="TabGroup" component={TabGroup} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator initialRouteName='TabGroup'>
+      <Stack.Screen name="TabGroup" component={TabGroup} options={{ headerShown: false }} />
+      <Stack.Screen name='TweetDetail' component={TweetDetail} options={{ presentation: 'modal' }} />
+    </Stack.Navigator>
   )
 }
+
+// function DrawerGroup () {
+//   <Drawer.Navigator>
+//     <Drawer.Screen name='' component={} />
+//   </Drawer.Navigator>
+// }
 
 function TabGroup () {
   return (
