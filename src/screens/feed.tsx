64b26b1ab5113b983256a559a6a485b68e91ade8
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-09-18 18:03:26 -0600

    feat: add screens

--- src/screens/feed.tsx
+++ src/screens/feed.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import { FlatList, StyleSheet, View } from 'react-native'
+import { tweets } from '../api/tweets'
+import { Tweet } from '../components/tweet'
 
 export const Feed = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
+    <View style={styles.container}>
+      <FlatList
+        data={tweets}
+        renderItem={({ item }) => <Tweet tweet={item} />}
+        keyExtractor={item => item.id}
+        ItemSeparatorComponent={() => <View style={styles.divider} />}
+      />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  },
+  divider: {
+    width: '100%',
+    height: StyleSheet.hairlineWidth,
+    backgroundColor: '#DDD',
+  }
+})
