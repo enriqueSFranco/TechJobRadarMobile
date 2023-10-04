cb1c539112cf4d0eaa01d759e74a0853641a60aa
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-10-04 14:51:37 -0600

    feat: add hook useForm

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,16 +1,9 @@
 import { useState } from "react"
 
-interface FormState<T> {
-  [key: string]: T
-}
+export function useForm<T> (initialState: T) {
+  const [form, setForm] = useState<T>(initialState)
 
-type ChangeHandler<T> = (name: string, value: T) => void
-
-// TODO: HOOK PARA EL MANEJO DE FORMULARIOS
-export function useForm<T> () {
-  const [form, setForm] = useState<FormState<T>>({})
-
-  function handleChange<T> (name: string, value: T) {
+  function handleChange<K extends keyof T> (name: K, value: T[K]) {
     setForm(prevForm => ({
       ...prevForm,
       [name]: value
