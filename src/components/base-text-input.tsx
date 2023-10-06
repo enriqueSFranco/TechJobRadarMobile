1b3cddcca84dfae1364f11cdaf2f8da06e4cb325
enriqueSFranco <enrique.sfranco04@gmail.com>
2023-10-06 16:16:12 -0600

    feat: add BaseTextInput component

--- src/components/base-text-input.tsx
+++ src/components/base-text-input.tsx
@@ -1,3 +1,4 @@
+import { useId } from 'react'
 import { NativeSyntheticEvent, StyleSheet, TextInput, TextInputChangeEventData, Text, View, TextInputProps } from 'react-native'
 
 
@@ -9,18 +10,24 @@ type BaseTextInputProps = {
   onChange: (e: NativeSyntheticEvent<TextInputChangeEventData>) => void
 }
 
-export const BaseTextInput = ({ prompt, label, value, icon, onChange, ...rest }: BaseTextInputProps & TextInputProps) => {
+export const BaseTextInput = ({ prompt = '', label = '', value, icon, onChange, ...rest }: BaseTextInputProps & TextInputProps) => {
+  const inputWithId = useId()
+
   return (
     <View style={[styles.box, { gap: label !== undefined ? 8 : 0 }]}>
-      {label !== undefined ? <Text>{label}</Text> : null}
-      <TextInput
-        placeholder={prompt}
-        value={value}
-        onChange={onChange}
-        {...rest}
-        style={{ borderWidth: 0.5, borderColor: '#777', borderRadius: 4, paddingVertical: 14, paddingHorizontal: 8 }}
-      />
-      {icon !== undefined ? icon : null}
+      {label !== undefined ? <Text style={styles.label}>{label}</Text> : null}
+      <View style={styles.boxInput}>
+        <TextInput
+          nativeID={inputWithId}
+          placeholder={prompt}
+          value={value}
+          onChange={onChange}
+          accessibilityLabel={label || prompt}
+          {...rest}
+          style={styles.field}
+        />
+        {icon !== undefined ? icon : null}
+      </View>
     </View>
   )
 }
@@ -29,5 +36,25 @@ const styles = StyleSheet.create({
   box: {
     backgroundColor: '#fff',
     justifyContent: 'center',
+  },
+  boxInput: {
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    gap: 4,
+    backgroundColor: '#F4F4F4',
+    borderRadius: 8,
+    paddingVertical: 14,
+    paddingHorizontal: 8
+  },
+  field: {
+    flex: 1,
+    height: '100%',
+    paddingHorizontal: 8
+  },
+  label: {
+    fontWeight: '600',
+    color: '#222'
   }
 })
