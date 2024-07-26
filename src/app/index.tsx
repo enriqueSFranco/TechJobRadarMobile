15ce3565224dc617b9579d8af80b84458b473fd9
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-07-26 15:21:55 -0600

    chore: delete files

--- src/app/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { ListPizza } from "@components/organisms/ListPizza";
-import products from "@assets/data/products";
-
-export default function Index() {
-  return <ListPizza pizzas={products} />;
-}
