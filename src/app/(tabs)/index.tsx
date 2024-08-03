bbbc087067cf63e18d6f971bc802eed617d48ac8
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-08-02 18:07:25 -0600

    feat(): add Redirect component

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,15 +1,5 @@
-import { Link, router } from "expo-router";
-import React from "react";
-import { Pressable, Text, View } from "react-native";
+import { Redirect } from "expo-router";
 
-export default function Home() {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>hello world</Text>
-      <Link href="/settings">go to settings</Link>
-      <Pressable onPress={() => router.push("/settings")}>
-        <Text>go to settings with router push</Text>
-      </Pressable>
-    </View>
-  );
+export default function TabIndex() {
+  return <Redirect href="/menu/" />;
 }
