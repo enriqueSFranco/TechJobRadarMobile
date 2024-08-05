b4c0bfeaee3b6605accb8a5fdfa29f8f3d7d0867
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-08-05 17:55:27 -0600

    feat(): cart screen in modal mode

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,19 +1,21 @@
 import { Stack } from "expo-router";
+import { Colors } from "@/shared/constants.d";
 
 export default function Layout() {
   return (
     <Stack
       screenOptions={{
         headerStyle: {
-          backgroundColor: "#f4511e",
+          backgroundColor: Colors.dark.background,
         },
-        headerTintColor: "#fff",
+        headerTintColor: Colors.dark.color,
         headerTitleStyle: {
           fontWeight: "bold",
         },
       }}
     >
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="cart" options={{ presentation: "modal" }} />
     </Stack>
   );
 }
