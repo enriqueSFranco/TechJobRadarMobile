106f4758303b0b3f707a5b1611287f8871f04c49
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-08-05 17:56:19 -0600

    feat(): The cart icon was added to the menu screen

--- src/app/(tabs)/menu/_layout.tsx
+++ src/app/(tabs)/menu/_layout.tsx
@@ -1,9 +1,30 @@
-import { Stack } from "expo-router";
+import { Link, Stack } from "expo-router";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { ButtonPressable } from "@/components/atoms/ButtonPressable";
 
 export default function MenuStack() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: "Menu" }} />
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "Menu",
+          headerRight: () => (
+            <Link href="/cart" asChild>
+              <ButtonPressable
+                text={
+                  <MaterialIcons name="shopping-cart" size={24} color="black" />
+                }
+                style={{
+                  backgroundColor: "transparent",
+                  width: "auto",
+                  height: "auto",
+                }}
+              />
+            </Link>
+          ),
+        }}
+      />
     </Stack>
   );
 }
