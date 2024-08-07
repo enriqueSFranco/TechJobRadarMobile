e48d5dacf3373cd8d6d9ae7bc044a0c9cf79f757
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-08-06 18:59:29 -0600

    feat(): privider for shopping cart

--- src/provider/ShoppingCartProvider.tsx
+++ src/provider/ShoppingCartProvider.tsx
@@ -1,9 +1,10 @@
 import { createContext, useState } from "react";
+import { randomUUID } from "expo-crypto";
 import { CartItem, Pizza } from "@/shared/types.d";
 
 export type ShoppingCartContexType = {
   cart: CartItem[];
-  addItem: (product: Pizza, pizzaSize: Pizza["size"]) => void;
+  addItem: (product: Pizza, pizzaSize: CartItem["size"]) => void;
 };
 
 export const ShoppingCartContex = createContext<ShoppingCartContexType | null>(
@@ -16,15 +17,31 @@ type ShoppingCartProviderProps = {
 export function ShoppingCartProvider ({ children }: ShoppingCartProviderProps) {
   const [cart, updateCart] = useState<CartItem[]>([]);
 
-  function addItem (product: Pizza, pizzaSize: Pizza["size"]) {
-    const newProduct: CartItem = {
-      id: crypto.randomUUID(),
-      product: product,
-      product_id: product.id,
-      size: pizzaSize,
-      quantity: 1,
-    };
-    updateCart((prevCart) => [...prevCart, newProduct]);
+  function addItem (product: Pizza, pizzaSize: CartItem["size"]) {
+    // caso 0: Verificar si el producto ya esta agregado
+    const productIdx = cart.findIndex(
+      (it) => it.product_id === product.id && it.size === pizzaSize,
+    );
+    // caso 1: Si el producto ya está agregado, entonces aumentamos el valor de la propiedad `quantity`
+    if (productIdx !== -1) {
+      updateCart((prevCart) => updateQuantity(prevCart, productIdx));
+    } else {
+      // caso 2: Si el producto no está agregado, entonces se agrega al carrito
+      const newProduct: CartItem = {
+        id: randomUUID(),
+        product: product,
+        product_id: product.id,
+        size: pizzaSize,
+        quantity: 1,
+      };
+      updateCart((prevCart) => [...prevCart, newProduct]);
+    }
+  }
+
+  function updateQuantity (currCart: CartItem[], productIdx: number) {
+    return currCart.map((item, index) =>
+      index === productIdx ? { ...item, quantity: item.quantity++ } : item,
+    );
   }
 
   const data: ShoppingCartContexType = { cart, addItem };
