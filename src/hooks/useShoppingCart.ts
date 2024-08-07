3351fe1f7fc83e7b5d895ee7026c58bc076ebfd9
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-08-06 18:59:15 -0600

    feat(): hook for shopping cart

--- src/hooks/useShoppingCart.ts
+++ src/hooks/useShoppingCart.ts
@@ -4,12 +4,12 @@ import {
 } from "@/provider/ShoppingCartProvider";
 import { useContext } from "react";
 
-export function useShoppingCart (): ShoppingCartContexType | null {
+export function useShoppingCart (): ShoppingCartContexType {
   const ctx = useContext(ShoppingCartContex);
 
   if (!ctx) {
     throw new Error(
-      "useShoppingCart debe ser usado dentro de un proveedor de PizzaSizeContext.",
+      "useShoppingCart debe ser usado dentro de un proveedor de ShoppingCartContex.",
     );
   }
   return ctx;
