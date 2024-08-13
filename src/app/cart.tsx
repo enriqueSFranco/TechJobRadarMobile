15ed8329d9d3d2da2e706ff5415e8cb3c5b6f1df
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-08-13 15:58:03 -0600

    feat(app/user): user routes

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -3,19 +3,27 @@ import { StatusBar } from "expo-status-bar";
 import { useShoppingCart } from "@/hooks/useShoppingCart";
 import { CardPizzaOrder } from "@/components/organisms/CardPizzaOrder";
 import { styles as globalStyles } from "@/styles/globalStyles";
+import { ButtonPressable } from "@/components/atoms/ButtonPressable";
 
 export default function Cart () {
-  const { cart } = useShoppingCart();
+  const { cart, total } = useShoppingCart();
 
   return (
     <View style={globalStyles.container}>
       <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
       <FlatList
         data={cart}
+        ListEmptyComponent={
+          <View>
+            <Text>carrito vacio</Text>
+          </View>
+        }
         renderItem={({ item }) => <CardPizzaOrder order={item} />}
         keyExtractor={(item) => item.id}
         contentContainerStyle={{ gap: 10 }}
       />
+      <Text>Total: {total}</Text>
+      <ButtonPressable text="checkout" />
     </View>
   );
 }
