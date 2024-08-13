640c8106f046461b0f1ffa55b87c388489658130
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-08-13 15:58:47 -0600

    feat(providers): add provider shopping cart

--- src/provider/ShoppingCartProvider.tsx
+++ src/provider/ShoppingCartProvider.tsx
@@ -1,10 +1,18 @@
-import { createContext, useState } from "react";
+import { createContext, useMemo, useState } from "react";
 import { randomUUID } from "expo-crypto";
-import { CartItem, Pizza } from "@/shared/types.d";
+import { CartItem, Pizza, PizzaSize } from "@/shared/types.d";
+import { QuantityChange } from "@/shared/enums.d";
+import { formatMoney } from "@/helpers/format-money";
 
 export type ShoppingCartContexType = {
   cart: CartItem[];
   addItem: (product: Pizza, pizzaSize: CartItem["size"]) => void;
+  updateQuantity: (
+    itemId: string,
+    amount: QuantityChange.Increment | QuantityChange.Decrement,
+    size: PizzaSize,
+  ) => void;
+  total: string;
 };
 
 export const ShoppingCartContex = createContext<ShoppingCartContexType | null>(
@@ -18,13 +26,15 @@ export function ShoppingCartProvider ({ children }: ShoppingCartProviderProps) {
   const [cart, updateCart] = useState<CartItem[]>([]);
 
   function addItem (product: Pizza, pizzaSize: CartItem["size"]) {
+    console.log(cart, product, pizzaSize);
     // caso 0: Verificar si el producto ya esta agregado
     const productIdx = cart.findIndex(
       (it) => it.product_id === product.id && it.size === pizzaSize,
     );
     // caso 1: Si el producto ya está agregado, entonces aumentamos el valor de la propiedad `quantity`
     if (productIdx !== -1) {
-      updateCart((prevCart) => updateQuantity(prevCart, productIdx));
+      updateQuantity(String(productIdx), QuantityChange.Increment, pizzaSize);
+      return;
     } else {
       // caso 2: Si el producto no está agregado, entonces se agrega al carrito
       const newProduct: CartItem = {
@@ -38,13 +48,58 @@ export function ShoppingCartProvider ({ children }: ShoppingCartProviderProps) {
     }
   }
 
-  function updateQuantity (currCart: CartItem[], productIdx: number) {
-    return currCart.map((item, index) =>
-      index === productIdx ? { ...item, quantity: item.quantity++ } : item,
-    );
+  function updateItemQuantity ({
+    item,
+    itemId,
+    amount,
+    size,
+  }: {
+    item: CartItem;
+    itemId: string;
+    amount: QuantityChange.Increment | QuantityChange.Decrement;
+    size: PizzaSize;
+  }) {
+    console.log(item.product_id, itemId, size, item.size);
+    return item.product_id === parseInt(itemId) && item.size === size
+      ? { ...item, quantity: item.quantity + amount }
+      : item;
   }
 
-  const data: ShoppingCartContexType = { cart, addItem };
+  function filterItemsWithPositiveQuantity (items: CartItem[]) {
+    return items.filter((item) => item.quantity > 0);
+  }
+
+  function updateQuantity (
+    itemId: string,
+    amount: QuantityChange.Increment | QuantityChange.Decrement,
+    size: PizzaSize,
+  ) {
+    console.log(">>>updateQuantity");
+    const updatedCart = cart.map((item) => {
+      return updateItemQuantity({ item, itemId, amount, size });
+    });
+    const filteredCart = filterItemsWithPositiveQuantity(updatedCart);
+    console.log(filteredCart);
+    updateCart(filteredCart);
+  }
+
+  const total = useMemo(() => {
+    let total = 0;
+    for (const item of cart) {
+      const { quantity, product } = item;
+      const priceInt = product.price as number;
+
+      total += priceInt * quantity;
+    }
+    return formatMoney({ value: total });
+  }, [cart]);
+
+  const data: ShoppingCartContexType = {
+    cart,
+    addItem,
+    updateQuantity,
+    total,
+  };
   return (
     <ShoppingCartContex.Provider value={data}>
       {children}
