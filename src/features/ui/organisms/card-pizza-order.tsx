e9a53fc6f56148877403b68598c7af83dbb6ff50
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-08-15 13:52:00 -0600

    feat(features/ui): rename files

---
+++ src/features/ui/organisms/card-pizza-order.tsx
@@ -0,0 +1,114 @@
+import { CartItem } from "@/shared/types.d";
+import { Image, StyleSheet, Text, View } from "react-native";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import { Colors } from "@/shared/constants.d";
+import { formatMoney } from "@/helpers/format-money";
+import { QuantityChange } from "@/shared/enums.d";
+import { IconButton } from "../atoms/icon-button";
+import { useShoppingCart } from "@/hooks/useShoppingCart";
+
+type CardPizzaOrderProps = {
+  order: CartItem;
+};
+
+export function CardPizzaOrder ({ order }: CardPizzaOrderProps) {
+  const { updateQuantity } = useShoppingCart();
+  const { product, quantity, size } = order;
+  const { image, name, price, id } = product;
+  const formattedPrice = formatMoney({ value: price });
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.productContainer}>
+        <Image source={{ uri: image }} style={styles.image} />
+        <View>
+          <Text style={styles.name}>{name}</Text>
+          <View style={styles.infoContainer}>
+            <Text style={styles.price}>{formattedPrice}</Text>
+            <Text style={styles.size}>Size: {size}</Text>
+          </View>
+        </View>
+      </View>
+      <View style={styles.controlsContainer}>
+        <IconButton
+          onPress={() =>
+            updateQuantity(String(id), QuantityChange.Increment, size)
+          }
+        >
+          <MaterialIcons name="add" size={18} color="black" />
+        </IconButton>
+        <Text>{quantity}</Text>
+        <IconButton
+          onPress={() =>
+            updateQuantity(String(id), QuantityChange.Decrement, size)
+          }
+        >
+          <AntDesign name="minus" size={18} color="black" />
+        </IconButton>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 8,
+    backgroundColor: Colors.light.background,
+    borderRadius: 8,
+  },
+  productContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 12,
+  },
+  image: {
+    width: 80,
+    height: 80,
+  },
+  name: {
+    color: Colors.light.color,
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  infoContainer: {
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    gap: 12,
+    marginTop: 4,
+  },
+  price: {
+    color: "#09F",
+    fontWeight: "600",
+    fontSize: 16,
+  },
+  size: {
+    fontSize: 14,
+    color: "#333",
+  },
+  controlsContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 12,
+  },
+  button: {
+    width: 30,
+    height: 30,
+    backgroundColor: "transparent",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    color: Colors.light.color,
+    fontSize: 18,
+  },
+  quantity: {
+    fontSize: 16,
+    color: "#333",
+  },
+});
---
+++ src/features/ui/organisms/card-pizza.tsx
@@ -0,0 +1,45 @@
+import { Image, Pressable, StyleSheet, Text } from "react-native";
+import { Link, useSegments } from "expo-router";
+import { Colors } from "@/shared/constants.d";
+import { Pizza } from "@/shared/types.d";
+import { formatMoney } from "@/helpers/format-money";
+
+type CardPizzaProps = {
+  pizza: Pizza;
+};
+
+export const CardPizza = ({ pizza }: CardPizzaProps) => {
+  const segments = useSegments();
+  const formattedPrice = formatMoney({ value: pizza.price });
+
+  return (
+    <Link href={`${segments[0]}/menu/${pizza.id}`} asChild>
+      <Pressable style={styles.container}>
+        <Image
+          source={{ uri: pizza.image }}
+          style={styles.image}
+          resizeMode="cover"
+        />
+        <Text>{pizza.name}</Text>
+        <Text style={styles.price}>{formattedPrice}</Text>
+      </Pressable>
+    </Link>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    maxWidth: "50%",
+    backgroundColor: Colors.light.background,
+    padding: 10,
+    borderRadius: 8,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  price: {
+    color: Colors.light.tint,
+  },
+});
