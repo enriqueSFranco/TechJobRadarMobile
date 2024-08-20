9d257f2a55e091b29ff4282b883d451b124ec090
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-08-20 16:34:05 -0600

    feat(app/(admin)/menu): add mode edit product

--- src/app/(admin)/menu/create.tsx
+++ src/app/(admin)/menu/create.tsx
@@ -1,15 +1,45 @@
-import { Button, Image, Text, TextInput, View } from "react-native";
-import { useState } from "react";
+import {
+  Alert,
+  Button,
+  Image,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import * as ImagePicker from "expo-image-picker";
-import { Pizza } from "@/shared/types.d";
+import { useState } from "react";
+import { styles as globalStyles } from "@/styles/globalStyles";
+import { useStock } from "@/hooks/useStock";
+import { Colors } from "@/shared/constants.d";
+import { Stack, useLocalSearchParams } from "expo-router";
+import { title } from "process";
+import { text } from "stream/consumers";
+
+const INITIAL_PRODUCT_FORM_VALUES: ProductFormValues = {
+  name: "",
+  price: "",
+};
+
+type ProductFormValues = {
+  name: string;
+  price: string;
+};
 
 const urlDefaultPizzaImage =
   "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
 
 export default function Create () {
-  const [name, setName] = useState<string>("");
-  const [price, setPrice] = useState<string>("");
+  const [form, setForm] = useState<ProductFormValues>({
+    name: "",
+    price: "",
+  });
   const [image, setImage] = useState<string | null>(null);
+  const { id } = useLocalSearchParams();
+  const { createProduct, updateProduct } = useStock();
+
+  const isEditMode = id !== undefined;
 
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -19,19 +49,58 @@ export default function Create () {
       quality: 1,
     });
 
-    console.log(result);
-
     if (!result.canceled) setImage(result.assets[0].uri);
   };
 
+  function handleFormReset () {
+    setForm(INITIAL_PRODUCT_FORM_VALUES);
+  }
+
+  function handleChangeText<T> (name: string, newText: T) {
+    setForm((prevForm) => ({
+      ...prevForm,
+      [name]: newText,
+    }));
+  }
+
   // TODO: implementar la funcion de crear una pizza
   function _handleCreate () {
+    const { name, price } = form;
     // validar la información ingrasada por el usuario
-    if (!name.trim() || !price.trim()) return;
+    createProduct({ name, price, image });
+    handleFormReset();
+  }
+
+  function _handleUpdate (id: string) {
+    const { name, price } = form;
+
+    // validar la información ingrasada por el usuario
+    updateProduct(id);
+    // handleFormReset();
+  }
+
+  function handleDelete () {
+    console.log("DELETE PRODUCT");
+  }
+
+  function _confirm () {
+    Alert.alert("Confirm", "Are you sure you want to delete this product", [
+      { text: "Cancel" },
+      { text: "Delete", style: "destructive", onPress: handleDelete },
+    ]);
+  }
+
+  function handleSubmit () {
+    if (isEditMode) {
+      _handleUpdate(String(id));
+    } else _handleCreate();
   }
 
   return (
-    <View>
+    <View style={globalStyles.container}>
+      <Stack.Screen
+        options={{ title: isEditMode ? "Update Pizza" : "Create Pizza" }}
+      />
       <View>
         {/* input for upload image */}
         <Image
@@ -42,28 +111,79 @@ export default function Create () {
       </View>
 
       {/* form */}
-      <View>
+      <View style={styles.form}>
         {/* name */}
-        <View>
+        <View style={styles.boxInput}>
           <Text>Name</Text>
           <TextInput
-            value={name}
-            onChangeText={(newText) => setName(newText)}
+            placeholder="Cheesy BBQ"
+            style={styles.textField}
+            value={form.name}
+            onChangeText={(newText) => handleChangeText("name", newText)}
           />
         </View>
         {/* price */}
-        <View>
+        <View style={styles.boxInput}>
           <Text>Price ($)</Text>
           <TextInput
             placeholder="9.9"
             keyboardType="numeric"
-            value={price}
-            onChangeText={(newText) => setPrice(newText)}
+            style={styles.textField}
+            value={form.price}
+            onChangeText={(newText) => handleChangeText("price", newText)}
           />
         </View>
         {/* button create */}
-        <Button title="Create" onPress={_handleCreate} />
+        <Pressable onPress={_handleCreate} style={styles.btnCreate}>
+          <Text style={styles.btnCreateText}>
+            {isEditMode ? "Update" : "Create"}
+          </Text>
+        </Pressable>
+        {isEditMode && (
+          <Pressable onPress={_confirm} style={styles.btnCreate}>
+            <Text style={styles.btnCreateText}>
+              {isEditMode ? "Update" : "Create"}
+            </Text>
+          </Pressable>
+        )}
       </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  boxInput: {
+    width: "100%",
+    gap: 10,
+    justifyContent: "flex-start",
+  },
+
+  btnCreate: {
+    width: "100%",
+    backgroundColor: Colors.light.tint,
+    padding: 8,
+    height: 50,
+    borderRadius: 50,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  btnCreateText: {
+    color: Colors.dark.color,
+    fontSize: 16,
+    fontWeight: "bold",
+    textTransform: "capitalize",
+  },
+  form: {
+    width: "100%",
+    flex: 1,
+    gap: 16,
+  },
+
+  textField: {
+    borderColor: Colors.light.tint,
+    borderWidth: 1,
+    borderRadius: 6,
+    paddingVertical: 12,
+    paddingHorizontal: 6,
+  },
+});
