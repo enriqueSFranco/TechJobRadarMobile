6028f7ea31b2f45c076d6a0f0cbc1c685eb711da
enriqueSFranco <enrique.sfranco04@gmail.com>
2025-03-18 20:13:14 -0600

    chore: delete files

--- src/app/(admin)/orders/index.tsx
+++
@@ -1,6 +0,0 @@
-import { ListOrders } from "@/features/ui/organisms/list-orders";
-import fakeOrders from "@assets/data/orders";
-
-export default function Orders () {
-  return <ListOrders orders={fakeOrders} />;
-}
