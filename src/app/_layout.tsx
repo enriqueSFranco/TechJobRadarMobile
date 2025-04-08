e86f6788f1cbf39908b9e840e66d9be61b8de2d9
enriqueSFranco <enrique.sfranco04@gmail.com>
2025-04-07 18:40:05 -0600

    feat: keyboard avoiding view

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,32 @@
-import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { Text, KeyboardAvoidingView, Platform } from "react-native";
+import { ButtonPressable } from "@/components/atoms/button-pressable";
+import { TextField } from "@/components/atoms/text-field";
 
-export default function RootLayout() {
+export default function RootLayout () {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={{
+        flex: 1,
+        justifyContent: "flex-end",
+        gap: 32,
+        paddingHorizontal: 20,
+      }}
+    >
+      <Text style={{ fontSize: 24, fontWeight: "700" }}>Sign in</Text>
+      <TextField placeholder="Email" />
+      <TextField placeholder="Password" secureTextEntry />
+      <ButtonPressable
+        text="sign in"
+        textStyle={{
+          color: "#fff",
+          fontSize: 16,
+          fontWeight: "400",
+          textTransform: "capitalize",
+        }}
+      />
+      <StatusBar style="auto" />
+    </KeyboardAvoidingView>
   );
 }
